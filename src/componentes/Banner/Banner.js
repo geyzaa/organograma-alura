a4a0c4df3629c3a9a5dfc5af1e6e36f2f1bfb1b0
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-08-22 21:01:19 -0300

    primeiro componente

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+
+
+function Banner () {
+    //JSX parece html (como o react entende e transforma em elementos no DOM)
+    return (
+        <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+    )
+}
+
+export default Banner
