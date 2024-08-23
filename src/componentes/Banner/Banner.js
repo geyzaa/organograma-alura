75c4db8fc27684aaff6aa2a4bca8bdbe49dd9a8f
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-08-23 10:06:29 -0300

    componente banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,10 +1,12 @@
-
+import './Banner.css'
 
 function Banner () {
     //JSX parece html (como o react entende e transforma em elementos no DOM)
     return (
-        <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página do Organo"/>
+        </header>
     )
 }
 
-export default Banner
+export default Banner 
