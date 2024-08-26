cd54cb3aea4a2a51711c1153f665ca1ec162be08
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-08-26 15:03:24 -0300

    criando o formulário

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 e arquivo se chamam banner, no segundo só o nome da pasta aparece pq o arquivo 
 é nomeado como index.js, se tornando o principal e por tanto o importado pelo programa*/
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
+/*import CampoTexto from './componentes/CampoTexto';*/
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario/>
+      
     </div>
   );
 }
