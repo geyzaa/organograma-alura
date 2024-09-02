c1cf6729cb04011e63d966afc6d7fe1152ec4334
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-09-02 17:23:17 -0300

    cadastrando colaboradores

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 /*exemplos de como pode ser feito o importe, no primeiro a pasta 
 e arquivo se chamam banner, no segundo só o nome da pasta aparece pq o arquivo 
 é nomeado como index.js, se tornando o principal e por tanto o importado pelo programa*/
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 /*import CampoTexto from './componentes/CampoTexto';*/
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
       
     </div>
   );
