762b15c6561a1f42a6b76e0a58613a452687659b
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-09-09 19:24:42 -0300

    refatorando código duplicado

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
 
       {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     
