606a2882653c84cc0ec307a631b57962812dd60b
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-09-20 15:58:15 -0300

    card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'> 
+                <img src='https://github.com/geyzaa.png' alt='' />
+            </div>
+            <div className='rodape'>
+                <h4>Geyza</h4>
+                <h5> Instrutor </h5>
+            </div>
+
+        </div>
+    )
+}
+
+export default Colaborador
